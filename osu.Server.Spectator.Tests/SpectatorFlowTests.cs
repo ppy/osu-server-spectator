c62e706a2876d87a5b3fbaef22b3e8d75e5b1f90
Dean Herbert <pe@ppy.sh>
2020-10-21 16:52:23 +0900

    Extract variables for readability

--- osu.Server.Spectator.Tests/SpectatorFlowTests.cs
+++ osu.Server.Spectator.Tests/SpectatorFlowTests.cs
@@ -26,10 +26,14 @@ namespace osu.Server.Spectator.Tests
         public async Task NewUserConnectsAndStreamsData()
         {
             const string streamer_id = "1234";
+            const int beatmap_id = 88;
 
             Mock<IHubCallerClients<ISpectatorClient>> mockClients = new Mock<IHubCallerClients<ISpectatorClient>>();
-            Mock<ISpectatorClient> mockClientProxy = new Mock<ISpectatorClient>();
-            mockClients.Setup(clients => clients.All).Returns(mockClientProxy.Object);
+            Mock<ISpectatorClient> mockSender = new Mock<ISpectatorClient>();
+            Mock<ISpectatorClient> mockReceiver = new Mock<ISpectatorClient>();
+            mockClients.Setup(clients => clients.All).Returns(mockReceiver.Object);
+            mockClients.Setup(clients => clients.Caller).Returns(mockSender.Object);
+            mockClients.Setup(clients => clients.Group($"watch:{streamer_id}")).Returns(mockReceiver.Object);
 
             Mock<HubCallerContext> mockContext = new Mock<HubCallerContext>();
             mockContext.Setup(context => context.UserIdentifier).Returns(streamer_id);
@@ -41,11 +45,17 @@ namespace osu.Server.Spectator.Tests
 
             // check all other users were informed that streaming began
             mockClients.Verify(clients => clients.All, Times.Once);
-            mockClientProxy.Verify(clients => clients.UserBeganPlaying(streamer_id, 88), Times.Once);
+            mockReceiver.Verify(clients => clients.UserBeganPlaying(streamer_id, beatmap_id), Times.Once);
 
             // check state data was added
-            var state = await cache.GetStringAsync("state:1234");
-            Assert.Equal("88", state);
+            var state = await cache.GetStringAsync($"state:{streamer_id}");
+            Assert.Equal(beatmap_id.ToString(), state);
+
+            var data = new FrameDataBundle("test");
+            
+            // check streaming data is propagating to watchers
+            await hub.SendFrameData(data);
+            mockReceiver.Verify(clients => clients.UserSentFrames(streamer_id, data));
         }
     }
 }
