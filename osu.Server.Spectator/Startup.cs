9660f44f6a096320badb51dde80d512c928f80f5
Dean Herbert <pe@ppy.sh>
2020-10-26 13:06:58 +0900

    Add basic public key parsing for jwt

--- osu.Server.Spectator/Startup.cs
+++ osu.Server.Spectator/Startup.cs
@@ -1,13 +1,21 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
 using osu.Server.Spectator.Hubs;
 
 namespace osu.Server.Spectator
@@ -38,11 +46,33 @@ namespace osu.Server.Spectator
 
         protected virtual void ConfigureAuthentication(IServiceCollection services)
         {
+            var rsa = getKeyProvider();
+
             services.AddAuthentication(config =>
             {
                 config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
                 config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer();
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    IssuerSigningKey = new RsaSecurityKey(rsa),
+                    // TODO: check what "5" means.
+                    ValidAudience = "5",
+                    // TODO: figure out why this isn't included in the token.
+                    ValidateIssuer = false,
+                    ValidIssuer = "https://osu.ppy.sh/"
+                };
+
+                options.Events = new JwtBearerEvents
+                {
+                    OnAuthenticationFailed = context =>
+                    {
+                        Console.WriteLine("Failed auth");
+                        return Task.CompletedTask;
+                    },
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -61,6 +91,33 @@ namespace osu.Server.Spectator
             app.UseWebSockets();
 
             app.UseEndpoints(endpoints => { endpoints.MapHub<SpectatorHub>("/spectator"); });
+        }
+
+        /// <summary>
+        /// borrowed from https://stackoverflow.com/a/54323524
+        /// </summary>
+        private static RSACryptoServiceProvider getKeyProvider()
+        {
+            string key = File.ReadAllText("oauth-public.key");
+
+            key = key.Replace("-----BEGIN PUBLIC KEY-----", "");
+            key = key.Replace("-----END PUBLIC KEY-----", "");
+            key = key.Replace("\n", "");
+
+            var keyBytes = Convert.FromBase64String(key);
+
+            var asymmetricKeyParameter = PublicKeyFactory.CreateKey(keyBytes);
+            var rsaKeyParameters = (RsaKeyParameters)asymmetricKeyParameter;
+            var rsaParameters = new RSAParameters
+            {
+                Modulus = rsaKeyParameters.Modulus.ToByteArrayUnsigned(),
+                Exponent = rsaKeyParameters.Exponent.ToByteArrayUnsigned()
+            };
+
+            var rsa = new RSACryptoServiceProvider();
+            rsa.ImportParameters(rsaParameters);
+
+            return rsa;
         }
     }
 }
