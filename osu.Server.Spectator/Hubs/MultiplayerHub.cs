9a2c9991c72cf69c91ca409523984769ffb34388
Dean Herbert <pe@ppy.sh>
2020-12-04 17:13:43 +0900

    Remove local implementation of common state management logic

--- osu.Server.Spectator/Hubs/MultiplayerHub.cs
+++ osu.Server.Spectator/Hubs/MultiplayerHub.cs
@@ -1,44 +1,17 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
-using System.Collections.Concurrent;
-using System.Threading.Tasks;
 using JetBrains.Annotations;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Caching.Distributed;
 using osu.Game.Online.RealtimeMultiplayer;
 
 namespace osu.Server.Spectator.Hubs
 {
-    [UsedImplicitly]
-    [Authorize]
-    public class MultiplayerHub : Hub<IMultiplayerClient>, IMultiplayerServer
+    public class MultiplayerHub : StatefulUserHub<IMultiplayerClient, MultiplayerClientState>, IMultiplayerServer
     {
-        private readonly IDistributedCache cache;
-
-        private static readonly ConcurrentDictionary<int, MultiplayerRoomState> active_states = new ConcurrentDictionary<int, MultiplayerRoomState>();
-
-        public MultiplayerHub(IDistributedCache cache)
+        public MultiplayerHub([NotNull] IDistributedCache cache)
+            : base(cache)
         {
-            this.cache = cache;
-        }
-
-        private int currentContextUserId => int.Parse(Context.UserIdentifier);
-
-        public override async Task OnConnectedAsync()
-        {
-            Console.WriteLine($"User {currentContextUserId} connected!");
-
-            await base.OnConnectedAsync();
-        }
-
-        public override async Task OnDisconnectedAsync(Exception exception)
-        {
-            Console.WriteLine($"User {currentContextUserId} disconnected!");
-
-            await base.OnDisconnectedAsync(exception);
         }
     }
 }
