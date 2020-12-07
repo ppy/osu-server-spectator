83f57f396b73dd87a32837ec0587ea9f08a3a3b7
Dean Herbert <pe@ppy.sh>
2020-12-07 18:50:21 +0900

    Add basic room join / state tracking

--- osu.Server.Spectator/Hubs/MultiplayerHub.cs
+++ osu.Server.Spectator/Hubs/MultiplayerHub.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
 using Microsoft.Extensions.Caching.Distributed;
@@ -10,6 +11,9 @@ namespace osu.Server.Spectator.Hubs
 {
     public class MultiplayerHub : StatefulUserHub<IMultiplayerClient, MultiplayerClientState>, IMultiplayerServer
     {
+        // for the time being rooms will be maintained in memory and not distributed.
+        private static readonly Dictionary<long, MultiplayerRoom> active_rooms = new Dictionary<long, MultiplayerRoom>();
+
         public MultiplayerHub([NotNull] IDistributedCache cache)
             : base(cache)
         {
@@ -25,6 +29,22 @@ namespace osu.Server.Spectator.Hubs
                 return false;
             }
 
+            MultiplayerRoom? room;
+
+            lock (active_rooms)
+            {
+                // check whether we are already aware of this match.
+
+                if (!active_rooms.TryGetValue(roomId, out room))
+                {
+                    // TODO: get details of the room from the database. hard abort if non existent.
+                    active_rooms.Add(roomId, room = new MultiplayerRoom());
+                }
+            }
+
+            // add the user to the room.
+            room.Join(CurrentContextUserId);
+
             await Groups.AddToGroupAsync(Context.ConnectionId, GetGroupId(roomId));
             await UpdateLocalUserState(new MultiplayerClientState(roomId));
 
@@ -35,6 +55,21 @@ namespace osu.Server.Spectator.Hubs
 
         public async Task LeaveRoom(long roomId)
         {
+            var state = await GetLocalUserState();
+
+            if (state == null)
+                return;
+
+            MultiplayerRoom? room;
+
+            lock (active_rooms)
+            {
+                if (!active_rooms.TryGetValue(roomId, out room))
+                    return;
+            }
+
+            room.Leave(CurrentContextUserId);
+
             await Groups.RemoveFromGroupAsync(Context.ConnectionId, GetGroupId(roomId));
             await RemoveLocalUserState();
         }
