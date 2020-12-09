c6cb1856a584628a808ad1392635c7b945670ac5
Dean Herbert <pe@ppy.sh>
2020-12-09 12:46:23 +0900

    Add actual endpoint mapping

--- osu.Server.Spectator/Startup.cs
+++ osu.Server.Spectator/Startup.cs
@@ -92,6 +92,7 @@ namespace osu.Server.Spectator
             app.UseWebSockets();
 
             app.UseEndpoints(endpoints => { endpoints.MapHub<SpectatorHub>("/spectator"); });
+            app.UseEndpoints(endpoints => { endpoints.MapHub<MultiplayerHub>("/multiplayer"); });
         }
 
         /// <summary>
