54a68023947e79388ca1ab15fe3f6127ccde3b9c
smoogipoo <smoogipoo@smgi.me>
2020-12-16 15:40:09 +0900

    Remove duplicated code

--- osu.Server.Spectator/Program.cs
+++ osu.Server.Spectator/Program.cs
@@ -1,25 +1,13 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using MySqlConnector;
 
 namespace osu.Server.Spectator
 {
     public static class Program
     {
-        public static MySqlConnection GetDatabaseConnection()
-        {
-            string host = (Environment.GetEnvironmentVariable("DB_HOST") ?? "localhost");
-            string user = (Environment.GetEnvironmentVariable("DB_USER") ?? "root");
-
-            var connection = new MySqlConnection($"Server={host};Database=osu;User ID={user};ConnectionTimeout=1;ConnectionReset=false;Pooling=true;");
-            connection.Open();
-            return connection;
-        }
-
         public static void Main(string[] args)
         {
             createHostBuilder(args).Build().Run();
