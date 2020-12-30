a4f6c8308b5f347d8fabd122e0efb07e54a74e83
Dean Herbert <pe@ppy.sh>
2020-12-30 13:48:12 +0900

    Add test covering lock and wait behaviour

--- osu.Server.Spectator.Tests/EntityStoreTests.cs
+++ osu.Server.Spectator.Tests/EntityStoreTests.cs
@@ -2,6 +2,8 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 using osu.Server.Spectator.Hubs;
 using Xunit;
 
@@ -26,6 +28,41 @@ namespace osu.Server.Spectator.Tests
 
             using (var secondGet = await store.GetForUse(1))
             {
+                Assert.NotNull(secondGet.Item);
+                Assert.Equal("test data", secondGet.Item?.TestData);
+            }
+        }
+
+        [Fact]
+        public async void TestGetTwiceWithDelayedReturn()
+        {
+            var firstLockAchieved = new ManualResetEventSlim();
+            var firstLockDelayComplete = new ManualResetEventSlim();
+
+            new Thread(async () =>
+            {
+                using (var firstGet = await store.GetForUse(1))
+                {
+                    // signal the second fetch to start once the first lock has been achieved.
+                    firstLockAchieved.Set();
+
+                    firstGet.Item = new TestItem("test data");
+
+                    await Task.Delay(2000);
+
+                    firstLockDelayComplete.Set();
+                }
+            }).Start();
+
+            firstLockAchieved.Wait();
+
+            // the delay should not be over yet.
+            Assert.False(firstLockDelayComplete.IsSet);
+
+            using (var secondGet = await store.GetForUse(1))
+            {
+                Assert.True(firstLockDelayComplete.IsSet);
+
                 Assert.NotNull(secondGet.Item);
                 Assert.Equal("test data", secondGet.Item?.TestData);
             }
