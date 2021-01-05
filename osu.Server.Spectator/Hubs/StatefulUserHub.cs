c4badcace493dc7129ca295810a375280674fdd2
Dean Herbert <pe@ppy.sh>
2021-01-05 12:36:00 +0900

    Remove unused exception variable

--- osu.Server.Spectator/Hubs/StatefulUserHub.cs
+++ osu.Server.Spectator/Hubs/StatefulUserHub.cs
@@ -41,7 +41,7 @@ namespace osu.Server.Spectator.Hubs
                 // if a previous connection is still present for the current user, we need to clean it up.
                 await cleanUpState(false);
             }
-            catch (Exception e)
+            catch
             {
                 Log("State cleanup failed");
 
