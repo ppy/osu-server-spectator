9c2fdc7ca6b2eb91a68951f73a1b8398e38c8b70
Salman Ahmed <frenzibyte@gmail.com>
2021-07-27 13:54:19 +0300

    Move remaining entities class to `Entities` namespace

--- osu.Server.Spectator/Entities/UserHubEntities.cs
+++ osu.Server.Spectator/Entities/UserHubEntities.cs
@@ -1,9 +1,9 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using osu.Server.Spectator.Entities;
+using osu.Server.Spectator.Hubs;
 
-namespace osu.Server.Spectator.Hubs
+namespace osu.Server.Spectator.Entities
 {
     public abstract class UserHubEntities<TUserState>
         where TUserState : ClientState
