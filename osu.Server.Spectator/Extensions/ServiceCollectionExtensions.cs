637307d8d9b6abe529e59d9391a1592c255e0412
Dean Herbert <pe@ppy.sh>
2021-07-28 17:58:54 +0900

    Remove unused classes and update singleton extension method

--- osu.Server.Spectator/Extensions/ServiceCollectionExtensions.cs
+++ osu.Server.Spectator/Extensions/ServiceCollectionExtensions.cs
@@ -2,8 +2,10 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using Microsoft.Extensions.DependencyInjection;
+using osu.Game.Online.Rooms;
 using osu.Server.Spectator.Database;
 using osu.Server.Spectator.Entities;
+using osu.Server.Spectator.Hubs;
 
 namespace osu.Server.Spectator.Extensions
 {
@@ -11,8 +13,9 @@ namespace osu.Server.Spectator.Extensions
     {
         public static IServiceCollection AddHubEntities(this IServiceCollection serviceCollection)
         {
-            return serviceCollection.AddSingleton<MultiplayerHubEntities>()
-                                    .AddSingleton<SpectatorHubEntities>();
+            return serviceCollection.AddSingleton<EntityStore<SpectatorClientState>>()
+                                    .AddSingleton<EntityStore<MultiplayerClientState>>()
+                                    .AddSingleton<EntityStore<Room>>();
         }
 
         public static IServiceCollection AddDatabaseServices(this IServiceCollection serviceCollection)
