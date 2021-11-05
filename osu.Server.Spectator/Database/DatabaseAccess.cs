740d0a4efc551a928ec8f9ad40581099f9592046
smoogipoo <smoogipoo@smgi.me>
2021-11-05 18:40:55 +0900

    Add queue mode update to database

--- osu.Server.Spectator/Database/DatabaseAccess.cs
+++ osu.Server.Spectator/Database/DatabaseAccess.cs
@@ -83,13 +83,14 @@ namespace osu.Server.Spectator.Database
 
         public async Task UpdateRoomSettingsAsync(MultiplayerRoom room)
         {
-            await connection.ExecuteAsync("UPDATE multiplayer_rooms SET name = @Name, password = @Password, type = @MatchType WHERE id = @RoomID", new
+            await connection.ExecuteAsync("UPDATE multiplayer_rooms SET name = @Name, password = @Password, type = @MatchType, queue_mode = @QueueMode WHERE id = @RoomID", new
             {
                 RoomID = room.RoomID,
                 Name = room.Settings.Name,
                 Password = room.Settings.Password,
                 // needs ToString() to store as enums correctly, see https://github.com/DapperLib/Dapper/issues/813.
                 MatchType = room.Settings.MatchType.ToDatabaseMatchType().ToString(),
+                QueueMode = room.Settings.QueueMode.ToDatabaseQueueModes().ToString()
             });
 
             var currentItem = await GetCurrentPlaylistItemAsync(room.RoomID);
