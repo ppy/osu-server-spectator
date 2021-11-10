0778711bdbb3abb31921f3cce6cbf2ad46fe3a46
smoogipoo <smoogipoo@smgi.me>
2021-11-10 16:22:35 +0900

    Add tests

--- osu.Server.Spectator.Tests/Multiplayer/MultiplayerQueueTests.cs
+++ osu.Server.Spectator.Tests/Multiplayer/MultiplayerQueueTests.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Threading.Tasks;
 using Moq;
 using osu.Game.Online.Multiplayer;
+using osu.Game.Online.Multiplayer.Queueing;
 using osu.Game.Online.Rooms;
 using osu.Game.Rulesets;
 using osu.Server.Spectator.Database.Models;
@@ -69,11 +70,70 @@ namespace osu.Server.Spectator.Tests.Multiplayer
             }
         }
 
+        #region Host-Only Mode
+
+        [Fact]
+        public async Task GuestsCannotAddItemsInHostOnlyMode()
+        {
+            Database.Setup(d => d.GetBeatmapChecksumAsync(3333)).ReturnsAsync("3333");
+            await Hub.JoinRoom(ROOM_ID);
+
+            SetUserContext(ContextUser2);
+            await Hub.JoinRoom(ROOM_ID);
+
+            await Assert.ThrowsAsync<NotHostException>(() => Hub.AddPlaylistItem(new APIPlaylistItem
+            {
+                BeatmapID = 3333,
+                BeatmapChecksum = "3333"
+            }));
+        }
+
+        [Fact]
+        public async Task GuestsCannotRemoveItemsInHostOnlyMode()
+        {
+            Database.Setup(d => d.GetBeatmapChecksumAsync(3333)).ReturnsAsync("3333");
+            long playlistItemId = (await Hub.JoinRoom(ROOM_ID)).Settings.PlaylistItemId;
+
+            SetUserContext(ContextUser2);
+            await Hub.JoinRoom(ROOM_ID);
+
+            await Assert.ThrowsAsync<NotHostException>(() => Hub.RemovePlaylistItem(new APIPlaylistItem
+            {
+                ID = playlistItemId
+            }));
+        }
+
+        [Fact]
+        public async Task AddingItemUpdatesExistingInHostOnlyMode()
+        {
+            Database.Setup(d => d.GetBeatmapChecksumAsync(3333)).ReturnsAsync("3333");
+
+            long playlistItemId = (await Hub.JoinRoom(ROOM_ID)).Settings.PlaylistItemId;
+
+            var newItem = new APIPlaylistItem
+            {
+                BeatmapID = 3333,
+                BeatmapChecksum = "3333"
+            };
+
+            await Hub.AddPlaylistItem(newItem);
+
+            using (var usage = Hub.GetRoom(ROOM_ID))
+            {
+                var room = usage.Item;
+                Debug.Assert(room != null);
+
+                Assert.Equal(playlistItemId, room.Settings.PlaylistItemId);
+                Database.Verify(db => db.UpdatePlaylistItemAsync(It.IsAny<multiplayer_playlist_item>()), Times.Once);
+                Database.Verify(db => db.AddPlaylistItemAsync(It.IsAny<multiplayer_playlist_item>()), Times.Never);
+                Receiver.Verify(r => r.PlaylistItemChanged(newItem), Times.Once);
+            }
+        }
+
         [Fact]
         public async Task RoomHasNewPlaylistItemAfterMatchStartInHostOnlyMode()
         {
-            long playlistItemId = (await Hub.JoinRoom(ROOM_ID)).Settings.PlaylistItemId;
-            long expectedPlaylistItemId = playlistItemId + 1;
+            long firstItemId = (await Hub.JoinRoom(ROOM_ID)).Settings.PlaylistItemId;
 
             await Hub.ChangeState(MultiplayerUserState.Ready);
             await Hub.StartMatch();
@@ -85,13 +145,92 @@ namespace osu.Server.Spectator.Tests.Multiplayer
                 var room = usage.Item;
                 Debug.Assert(room != null);
 
-                // Room has new playlist item.
-                Assert.Equal(expectedPlaylistItemId, room.Settings.PlaylistItemId);
-                Receiver.Verify(r => r.SettingsChanged(room.Settings), Times.Once);
+                var newItem = await room.QueueImplementation.GetCurrentItem(Database.Object);
 
-                // Playlist item is expired.
-                Assert.True((await Database.Object.GetPlaylistItemFromRoomAsync(ROOM_ID, playlistItemId))!.expired);
+                // Room has new playlist item.
+                Assert.NotEqual(firstItemId, newItem.id);
+                Assert.Equal(newItem.id, room.Settings.PlaylistItemId);
+
+                // Previous item is expired.
+                Assert.True((await Database.Object.GetPlaylistItemFromRoomAsync(ROOM_ID, firstItemId))!.expired);
+
+                // Players received callbacks.
+                Receiver.Verify(r => r.PlaylistItemAdded(It.Is<APIPlaylistItem>(i => i.ID == newItem.id)), Times.Once);
+                Receiver.Verify(r => r.PlaylistItemChanged(It.Is<APIPlaylistItem>(i => i.ID == firstItemId && i.Expired)), Times.Once);
+                Receiver.Verify(r => r.SettingsChanged(room.Settings), Times.Once);
             }
         }
+
+        [Fact]
+        public async Task AddingItemDoesNotAffectPastItemsInHostOnlyMode()
+        {
+            Database.Setup(d => d.GetBeatmapChecksumAsync(3333)).ReturnsAsync("3333");
+
+            long firstItemId = (await Hub.JoinRoom(ROOM_ID)).Settings.PlaylistItemId;
+
+            await Hub.ChangeState(MultiplayerUserState.Ready);
+            await Hub.StartMatch();
+            await Hub.ChangeState(MultiplayerUserState.Loaded);
+            await Hub.ChangeState(MultiplayerUserState.FinishedPlay);
+
+            var newItem = new APIPlaylistItem
+            {
+                BeatmapID = 3333,
+                BeatmapChecksum = "3333"
+            };
+
+            await Hub.AddPlaylistItem(newItem);
+
+            using (var usage = Hub.GetRoom(ROOM_ID))
+            {
+                var room = usage.Item;
+                Debug.Assert(room != null);
+
+                var firstItem = (await Database.Object.GetPlaylistItemFromRoomAsync(ROOM_ID, firstItemId))!;
+                var currentItem = await room.QueueImplementation.GetCurrentItem(Database.Object);
+
+                // Current item changed.
+                Assert.Equal(newItem.BeatmapID, currentItem.beatmap_id);
+                Receiver.Verify(r => r.PlaylistItemChanged(It.Is<APIPlaylistItem>(i => i.ID == currentItem.id && i.BeatmapID == newItem.BeatmapID)), Times.Once);
+
+                // Previous item unchanged.
+                Assert.Equal(1234, firstItem.beatmap_id);
+                Receiver.Verify(r => r.PlaylistItemChanged(It.Is<APIPlaylistItem>(i => i.ID == firstItem.id && i.BeatmapID != 1234)), Times.Never);
+            }
+        }
+
+        #endregion
+
+        #region Free-For-All Mode
+
+        [Fact]
+        public async Task AddingItemDoesNotReplaceExistingInFreeForAllMode()
+        {
+            Database.Setup(d => d.GetBeatmapChecksumAsync(3333)).ReturnsAsync("3333");
+
+            long playlistItemId = (await Hub.JoinRoom(ROOM_ID)).Settings.PlaylistItemId;
+            await Hub.ChangeSettings(new MultiplayerRoomSettings { QueueMode = QueueModes.FreeForAll });
+
+            var newItem = new APIPlaylistItem
+            {
+                BeatmapID = 3333,
+                BeatmapChecksum = "3333"
+            };
+
+            await Hub.AddPlaylistItem(newItem);
+
+            using (var usage = Hub.GetRoom(ROOM_ID))
+            {
+                var room = usage.Item;
+                Debug.Assert(room != null);
+
+                Assert.Equal(playlistItemId, room.Settings.PlaylistItemId);
+                Database.Verify(db => db.UpdatePlaylistItemAsync(It.IsAny<multiplayer_playlist_item>()), Times.Never);
+                Database.Verify(db => db.AddPlaylistItemAsync(It.IsAny<multiplayer_playlist_item>()), Times.Once);
+                Receiver.Verify(r => r.PlaylistItemAdded(newItem), Times.Once);
+            }
+        }
+
+        #endregion
     }
 }
