41bc1df7c4e7d625a7b5a2e760a5fe3a16bbf9f2
Dan Balasescu <smoogipoo@smgi.me>
2022-05-13 13:24:59 +0900

    Add round robin test with more users

--- osu.Server.Spectator.Tests/Multiplayer/MultiplayerAllPlayersRoundRobinQueueTests.cs
+++ osu.Server.Spectator.Tests/Multiplayer/MultiplayerAllPlayersRoundRobinQueueTests.cs
@@ -66,6 +66,172 @@ namespace osu.Server.Spectator.Tests.Multiplayer
             await checkCurrentItem(4);
         }
 
+        [Fact]
+        public async Task RoundRobinOrderingWithManyUsers()
+        {
+            Database.Setup(d => d.GetBeatmapChecksumAsync(3333)).ReturnsAsync("3333");
+
+            CreateUser(1, out var contextUser1, out _);
+            CreateUser(2, out var contextUser2, out _);
+            CreateUser(3, out var contextUser3, out _);
+            CreateUser(4, out var contextUser4, out _);
+
+            // ---
+            // User 1 joins and begins adding items.
+            // ---
+
+            SetUserContext(contextUser1);
+            await Hub.JoinRoom(ROOM_ID);
+            await Hub.ChangeSettings(new MultiplayerRoomSettings { QueueMode = QueueMode.AllPlayersRoundRobin });
+
+            // | item | user |
+            // | ---- | ---- |
+            // |  1   |  1   | (new)
+            await checkOrder(1);
+
+            // | item | user |
+            // | ---- | ---- |
+            // |  1   |  1   |
+            // |  2   |  1   | (new)
+            await addItem();
+            await checkOrder(1, 2);
+
+            // | item | user |
+            // | ---- | ---- |
+            // |  1   |  1   |
+            // |  2   |  1   |
+            // |  3   |  1   | (new)
+            await addItem();
+            await checkOrder(1, 2, 3);
+
+            // ---
+            // User 2 joins and begins adding items.
+            // ---
+
+            SetUserContext(contextUser2);
+            await Hub.JoinRoom(ROOM_ID);
+
+            // | item | user |
+            // | ---- | ---- |
+            // |  1   |  1   |
+            // |  4   |  2   | (new)
+            // |  2   |  1   |
+            // |  3   |  1   |
+            await addItem();
+            await checkOrder(1, 4, 2, 3);
+
+            // ---
+            // User 3 joins and begins adding items.
+            // ---
+
+            SetUserContext(contextUser3);
+            await Hub.JoinRoom(ROOM_ID);
+
+            // | item | user |
+            // | ---- | ---- |
+            // |  1   |  1   |
+            // |  4   |  2   |
+            // |  5   |  3   | (new)
+            // |  2   |  1   |
+            // |  3   |  1   |
+            await addItem();
+            await checkOrder(1, 4, 5, 2, 3);
+
+            // | item | user |
+            // | ---- | ---- |
+            // |  1   |  1   |
+            // |  4   |  2   |
+            // |  5   |  3   |
+            // |  2   |  1   |
+            // |  6   |  3   | (new)
+            // |  3   |  1   |
+            await addItem();
+            await checkOrder(1, 4, 5, 2, 6, 3);
+
+            // ---
+            // User 2 adds more items.
+            // ---
+
+            SetUserContext(contextUser2);
+
+            // | item | user |
+            // | ---- | ---- |
+            // |  1   |  1   |
+            // |  4   |  2   |
+            // |  5   |  3   |
+            // |  2   |  1   |
+            // |  7   |  2   | (new)
+            // |  6   |  3   |
+            // |  3   |  1   |
+            await addItem();
+            await checkOrder(1, 4, 5, 2, 7, 6, 3);
+
+            // | item | user |
+            // | ---- | ---- |
+            // |  1   |  1   |
+            // |  4   |  2   |
+            // |  5   |  3   |
+            // |  2   |  1   |
+            // |  7   |  2   |
+            // |  6   |  3   |
+            // |  3   |  1   |
+            // |  8   |  2   | (new)
+            await addItem();
+            await checkOrder(1, 4, 5, 2, 7, 6, 3, 8);
+
+            // ---
+            // User 4 joins and begins adding items.
+            // ---
+
+            SetUserContext(contextUser4);
+            await Hub.JoinRoom(ROOM_ID);
+
+            // | item | user |
+            // | ---- | ---- |
+            // |  1   |  1   |
+            // |  4   |  2   |
+            // |  5   |  3   |
+            // |  9   |  4   | (new)
+            // |  2   |  1   |
+            // |  7   |  2   |
+            // |  6   |  3   |
+            // |  3   |  1   |
+            // |  8   |  2   |
+            await addItem();
+            await checkOrder(1, 4, 5, 9, 2, 7, 6, 3, 8);
+
+            // | item | user |
+            // | ---- | ---- |
+            // |  1   |  1   |
+            // |  4   |  2   |
+            // |  5   |  3   |
+            // |  9   |  4   |
+            // |  2   |  1   |
+            // |  7   |  2   |
+            // |  6   |  3   |
+            // |  10  |  4   | (new)
+            // |  3   |  1   |
+            // |  8   |  2   |
+            await addItem();
+            await checkOrder(1, 4, 5, 9, 2, 7, 6, 10, 3, 8);
+
+            // | item | user |
+            // | ---- | ---- |
+            // |  1   |  1   |
+            // |  4   |  2   |
+            // |  5   |  3   |
+            // |  9   |  4   |
+            // |  2   |  1   |
+            // |  7   |  2   |
+            // |  6   |  3   |
+            // |  10  |  4   |
+            // |  3   |  1   |
+            // |  8   |  2   |
+            // |  11  |  4   | (new)
+            await addItem();
+            await checkOrder(1, 4, 5, 9, 2, 7, 6, 10, 3, 8, 11);
+        }
+
         private async Task runGameplay()
         {
             SetUserContext(ContextUser2);
