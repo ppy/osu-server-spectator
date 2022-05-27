d54513e3a0a0d539f934bd1c6bdd8a411df62319
Dean Herbert <pe@ppy.sh>
2022-05-27 15:57:00 +0900

    Remove locking and multiple-invocation support from `GracefulShutdownManager`

--- osu.Server.Spectator/GracefulShutdownManager.cs
+++ osu.Server.Spectator/GracefulShutdownManager.cs
@@ -22,25 +22,12 @@ public class GracefulShutdownManager
 
     private readonly List<IEntityStore> dependentStores = new List<IEntityStore>();
 
-    private Task? shutdownTask;
-
     public GracefulShutdownManager(EntityStore<ServerMultiplayerRoom> roomStore, EntityStore<SpectatorClientState> clientStateStore, IHostApplicationLifetime hostApplicationLifetime)
     {
         addDependentStore(roomStore);
         addDependentStore(clientStateStore);
 
-        hostApplicationLifetime.ApplicationStopping.Register(WaitForSafeShutdown);
-    }
-
-    /// <summary>
-    /// Blocks until safe to continue with shutdown. Can be invoked from multiple locations.
-    /// </summary>
-    public void WaitForSafeShutdown()
-    {
-        lock (this)
-            shutdownTask ??= Task.Factory.StartNew(shutdownSafely, TaskCreationOptions.LongRunning);
-
-        shutdownTask.Wait();
+        hostApplicationLifetime.ApplicationStopping.Register(shutdownSafely);
     }
 
     /// <summary>
@@ -52,39 +39,35 @@ public class GracefulShutdownManager
         if (store == null)
             return;
 
-        lock (dependentStores)
-            dependentStores.Add(store);
+        dependentStores.Add(store);
     }
 
     private void shutdownSafely()
     {
-        lock (dependentStores)
+        Logger.Log("Server shutdown triggered");
+
+        foreach (var store in dependentStores)
+            store.StopAcceptingEntities();
+
+        TimeSpan timeWaited = new TimeSpan();
+
+        TimeSpan timeBetweenChecks = TimeSpan.FromSeconds(10);
+
+        while (timeWaited < TIME_BEFORE_FORCEFUL_SHUTDOWN)
         {
-            Logger.Log("Server shutdown triggered");
+            var remaining = dependentStores.Select(store => (store.EntityName, store.RemainingUsages));
 
-            foreach (var store in dependentStores)
-                store.StopAcceptingEntities();
+            if (remaining.Sum(s => s.RemainingUsages) == 0)
+                break;
 
-            TimeSpan timeWaited = new TimeSpan();
+            Logger.Log("Waiting for usages of existing entities to finish...");
+            foreach (var r in remaining)
+                Logger.Log($"{r.EntityName,10}: {r.RemainingUsages}");
 
-            TimeSpan timeBetweenChecks = TimeSpan.FromSeconds(10);
-
-            while (timeWaited < TIME_BEFORE_FORCEFUL_SHUTDOWN)
-            {
-                var remaining = dependentStores.Select(store => (store.EntityName, store.RemainingUsages));
-
-                if (remaining.Sum(s => s.RemainingUsages) == 0)
-                    break;
-
-                Logger.Log("Waiting for usages of existing entities to finish...");
-                foreach (var r in remaining)
-                    Logger.Log($"{r.EntityName,10}: {r.RemainingUsages}");
-
-                Thread.Sleep(timeBetweenChecks);
-                timeWaited = timeWaited.Add(timeBetweenChecks);
-            }
-
-            Logger.Log("All entities cleaned up. Server shutdown unblocking.");
+            Thread.Sleep(timeBetweenChecks);
+            timeWaited = timeWaited.Add(timeBetweenChecks);
         }
+
+        Logger.Log("All entities cleaned up. Server shutdown unblocking.");
     }
 }
