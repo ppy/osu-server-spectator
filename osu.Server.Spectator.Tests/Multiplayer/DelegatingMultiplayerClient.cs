d7e4d9ee13a15853699aade1f79a47827209f62e
Dean Herbert <pe@ppy.sh>
2023-10-12 18:43:42 +0900

    Clean up implementation of `DelegatingMultiplayerClient` to not use ctor args

--- osu.Server.Spectator.Tests/Multiplayer/DelegatingMultiplayerClient.cs
+++ osu.Server.Spectator.Tests/Multiplayer/DelegatingMultiplayerClient.cs
@@ -19,135 +19,125 @@ namespace osu.Server.Spectator.Tests.Multiplayer
     /// </summary>
     public class DelegatingMultiplayerClient : IMultiplayerClient, IClientProxy
     {
-        private readonly IEnumerable<IMultiplayerClient> clients;
-
-        public DelegatingMultiplayerClient()
-            : this(Enumerable.Empty<IMultiplayerClient>())
-        {
-        }
-
-        public DelegatingMultiplayerClient(IEnumerable<IMultiplayerClient> clients)
-        {
-            this.clients = clients;
-        }
+        public virtual IEnumerable<IMultiplayerClient> Clients => Enumerable.Empty<IMultiplayerClient>();
 
         public virtual async Task RoomStateChanged(MultiplayerRoomState state)
         {
-            foreach (var c in clients)
+            foreach (var c in Clients)
                 await c.RoomStateChanged(state);
         }
 
         public virtual async Task UserJoined(MultiplayerRoomUser user)
         {
-            foreach (var c in clients)
+            foreach (var c in Clients)
                 await c.UserJoined(user);
         }
 
         public virtual async Task UserLeft(MultiplayerRoomUser user)
         {
-            foreach (var c in clients)
+            foreach (var c in Clients)
                 await c.UserLeft(user);
         }
 
         public virtual async Task UserKicked(MultiplayerRoomUser user)
         {
-            foreach (var c in clients)
+            foreach (var c in Clients)
                 await c.UserKicked(user);
         }
 
         public virtual async Task Invited(int invitedBy, long roomID, string password)
         {
-            foreach (var c in clients)
+            foreach (var c in Clients)
                 await c.Invited(invitedBy, roomID, password);
         }
 
         public virtual async Task HostChanged(int userId)
         {
-            foreach (var c in clients)
+            foreach (var c in Clients)
                 await c.HostChanged(userId);
         }
 
         public virtual async Task SettingsChanged(MultiplayerRoomSettings newSettings)
         {
-            foreach (var c in clients)
+            foreach (var c in Clients)
                 await c.SettingsChanged(newSettings);
         }
 
         public virtual async Task UserStateChanged(int userId, MultiplayerUserState state)
         {
-            foreach (var c in clients)
+            foreach (var c in Clients)
                 await c.UserStateChanged(userId, state);
         }
 
         public virtual async Task MatchUserStateChanged(int userId, MatchUserState state)
         {
-            foreach (var c in clients)
+            foreach (var c in Clients)
                 await c.MatchUserStateChanged(userId, state);
         }
 
         public virtual async Task MatchRoomStateChanged(MatchRoomState state)
         {
-            foreach (var c in clients)
+            foreach (var c in Clients)
                 await c.MatchRoomStateChanged(state);
         }
 
         public virtual async Task MatchEvent(MatchServerEvent e)
         {
-            foreach (var c in clients)
+            foreach (var c in Clients)
                 await c.MatchEvent(e);
         }
 
         public virtual async Task UserBeatmapAvailabilityChanged(int userId, BeatmapAvailability beatmapAvailability)
         {
-            foreach (var c in clients)
+            foreach (var c in Clients)
                 await c.UserBeatmapAvailabilityChanged(userId, beatmapAvailability);
         }
 
         public virtual async Task UserModsChanged(int userId, IEnumerable<APIMod> mods)
         {
-            foreach (var c in clients)
+            foreach (var c in Clients)
                 await c.UserModsChanged(userId, mods);
         }
 
         public virtual async Task LoadRequested()
         {
-            foreach (var c in clients)
+            foreach (var c in Clients)
                 await c.LoadRequested();
         }
 
         public virtual async Task LoadAborted()
         {
-            foreach (var c in clients)
+            foreach (var c in Clients)
                 await c.LoadAborted();
         }
 
         public virtual async Task GameplayStarted()
         {
-            foreach (var c in clients)
+            foreach (var c in Clients)
                 await c.GameplayStarted();
         }
 
         public virtual async Task ResultsReady()
         {
-            foreach (var c in clients)
+            foreach (var c in Clients)
                 await c.ResultsReady();
         }
 
         public virtual async Task PlaylistItemAdded(MultiplayerPlaylistItem item)
         {
-            foreach (var c in clients)
+            foreach (var c in Clients)
                 await c.PlaylistItemAdded(item);
         }
 
         public virtual async Task PlaylistItemRemoved(long playlistItemId)
         {
-            foreach (var c in clients)
+            foreach (var c in Clients)
                 await c.PlaylistItemRemoved(playlistItemId);
         }
 
         public virtual async Task PlaylistItemChanged(MultiplayerPlaylistItem item)
         {
-            foreach (var c in clients)
+            foreach (var c in Clients)
                 await c.PlaylistItemChanged(item);
         }
 
