7a21dbceb3af7c15e69fd1b69ba61640119edf82
Dean Herbert <pe@ppy.sh>
2023-10-12 18:45:44 +0900

    Remove incorrect implementation of `Invited` from `DelgatingMultiplayeClient`

--- osu.Server.Spectator.Tests/Multiplayer/DelegatingMultiplayerClient.cs
+++ osu.Server.Spectator.Tests/Multiplayer/DelegatingMultiplayerClient.cs
@@ -45,11 +45,7 @@ namespace osu.Server.Spectator.Tests.Multiplayer
                 await c.UserKicked(user);
         }
 
-        public virtual async Task Invited(int invitedBy, long roomID, string password)
-        {
-            foreach (var c in Clients)
-                await c.Invited(invitedBy, roomID, password);
-        }
+        public virtual Task Invited(int invitedBy, long roomID, string password) => Task.CompletedTask;
 
         public virtual async Task HostChanged(int userId)
         {
