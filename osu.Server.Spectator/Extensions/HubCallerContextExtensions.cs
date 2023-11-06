48e8b83f9ad17ed4c1294a1e31982137e884911c
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2023-11-06 13:35:45 +0100

    Add extension method for retrieving token ID

--- osu.Server.Spectator/Extensions/HubCallerContextExtensions.cs
+++ osu.Server.Spectator/Extensions/HubCallerContextExtensions.cs
@@ -18,5 +18,17 @@ namespace osu.Server.Spectator.Extensions
 
             return int.Parse(context.UserIdentifier);
         }
+
+        /// <summary>
+        /// Returns the ID of the authorisation token (more accurately, the <c>jti</c> claim)
+        /// for the supplied <paramref name="context"/>.
+        /// This is used for the purpose of identifying individual client instances
+        /// and preventing multiple concurrent sessions from being active.
+        /// </summary>
+        public static string GetTokenId(this HubCallerContext context)
+        {
+            return context.User?.FindFirst(claim => claim.Type == "jti")?.Value
+                   ?? throw new InvalidOperationException("Could not retrieve JWT ID claim from token");
+        }
     }
 }
