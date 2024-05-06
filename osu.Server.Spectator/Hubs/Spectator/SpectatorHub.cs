3723905645531affeba79d0aa3c92ca9e9577f29
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2024-05-06 17:54:49 +0200

    Ensure ranks are properly populated in spectated scores
    This begins to matter after https://github.com/ppy/osu/pull/28058, as
    with that pull the rank is encoded to the replay, so it must be
    correctly set on the server side. Otherwise spectator server will
    default to storing D rank, which the client will happily read and use
    raw without checking.
    
    This is purposefully computed server-side rather than sent in replay
    headers for the reasons of simplicity and backwards compatibility. You
    could add score rank to the replay headers, but to ensure that replays
    recorded with old clients don't have D rank set, recomputing server-side
    would have to be done at least for the old clients. And you could argue
    recomputing ranks always server-side removes a tamper vector too
    (although that's not a very strong argument).

--- osu.Server.Spectator/Hubs/Spectator/SpectatorHub.cs
+++ osu.Server.Spectator/Hubs/Spectator/SpectatorHub.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Caching.Distributed;
 using osu.Game.Beatmaps;
+using osu.Game.Database;
 using osu.Game.Online.Spectator;
 using osu.Game.Rulesets.Scoring;
 using osu.Game.Scoring;
@@ -116,6 +117,7 @@ namespace osu.Server.Spectator.Hubs.Spectator
                 if (score == null)
                     return;
 
+                score.ScoreInfo.Accuracy = data.Header.Accuracy;
                 score.ScoreInfo.Statistics = data.Header.Statistics;
                 score.ScoreInfo.MaxCombo = data.Header.MaxCombo;
                 score.ScoreInfo.Combo = data.Header.Combo;
@@ -166,6 +168,9 @@ namespace osu.Server.Spectator.Hubs.Spectator
                 return;
 
             score.ScoreInfo.Date = DateTimeOffset.UtcNow;
+            // this call is a little expensive due to reflection usage, so only run it at the end of score processing
+            // even though in theory the rank could be recomputed after every replay frame.
+            score.ScoreInfo.Rank = StandardisedScoreMigrationTools.ComputeRank(score.ScoreInfo);
 
             scoreUploader.Enqueue(scoreToken, score);
             await scoreProcessedSubscriber.RegisterForNotificationAsync(Context.ConnectionId, Context.GetUserId(), scoreToken);
