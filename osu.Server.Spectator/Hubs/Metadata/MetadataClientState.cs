2a9de42243fd84937eb74639fc004637bd47f9f4
Dean Herbert <pe@ppy.sh>
2025-01-17 15:27:52 +0900

    Add xmldoc to `ToUserPresence` to be very explicit about what `null` means

--- osu.Server.Spectator/Hubs/Metadata/MetadataClientState.cs
+++ osu.Server.Spectator/Hubs/Metadata/MetadataClientState.cs
@@ -19,6 +19,10 @@ namespace osu.Server.Spectator.Hubs.Metadata
             VersionHash = versionHash;
         }
 
+        /// <summary>
+        /// Creates a <see cref="UserPresence"/> which represents this user's state as it should be broadcast to other users.
+        /// </summary>
+        /// <returns>The representative user presence, or <c>null</c> if the user should appear offline.</returns>
         public UserPresence? ToUserPresence()
         {
             switch (UserStatus)
