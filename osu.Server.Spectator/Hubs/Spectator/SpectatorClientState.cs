5395794c057c7bf2da536732968ebc1bfaf5ee29
Dean Herbert <pe@ppy.sh>
2025-01-22 17:32:55 +0900

    Add xmldoc for `SpectatorClientState`

--- osu.Server.Spectator/Hubs/Spectator/SpectatorClientState.cs
+++ osu.Server.Spectator/Hubs/Spectator/SpectatorClientState.cs
@@ -11,10 +11,19 @@ namespace osu.Server.Spectator.Hubs.Spectator
     [Serializable]
     public class SpectatorClientState : ClientState
     {
+        /// <summary>
+        /// When a user is in gameplay, this is the state as conveyed at the start of the play session.
+        /// </summary>
         public SpectatorState? State;
 
+        /// <summary>
+        /// When a user is in gameplay, this is the imminent score. It will be updated throughout a play session.
+        /// </summary>
         public Score? Score;
 
+        /// <summary>
+        /// The score token as conveyed by the client at the beginning of a play session.
+        /// </summary>
         public long? ScoreToken;
 
         [JsonConstructor]
