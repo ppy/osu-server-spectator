610a9d6f9cf683f3d71e2bcc067a717175faf7f9
Dan Balasescu <smoogipoo@smgi.me>
2025-08-04 20:07:07 +0900

    Add basic single-gameplay flow

--- osu.Server.Spectator/Hubs/Multiplayer/Matchmaking/MatchmakingImplementation.cs
+++ osu.Server.Spectator/Hubs/Multiplayer/Matchmaking/MatchmakingImplementation.cs
@@ -1,7 +1,10 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Threading.Tasks;
+using osu.Game.Online.Matchmaking;
+using osu.Game.Online.Multiplayer;
 using osu.Game.Online.Multiplayer.MatchTypes.Matchmaking;
 using osu.Server.Spectator.Database.Models;
 
@@ -23,6 +26,58 @@ namespace osu.Server.Spectator.Hubs.Multiplayer.Matchmaking
         {
             await base.Initialise();
             await Hub.NotifyMatchRoomStateChanged(Room);
+        }
+
+        public override async Task HandleUserJoined(MultiplayerRoomUser user)
+        {
+            await base.HandleUserJoined(user);
+
+            if (Room.Users.Count == MATCHMAKING_ROOM_SIZE)
+            {
+                state.RoomStatus = MatchmakingRoomStatus.WaitForNextRound;
+                await Hub.NotifyMatchRoomStateChanged(Room);
+                await Room.StartCountdown(new MatchmakingStatusCountdown
+                {
+                    Status = state.RoomStatus,
+                    TimeRemaining = TimeSpan.FromSeconds(5)
+                }, beginNextRound);
+            }
+        }
+
+        private async Task beginNextRound(ServerMultiplayerRoom _)
+        {
+            state.RoomStatus = MatchmakingRoomStatus.Pick;
+            await Hub.NotifyMatchRoomStateChanged(Room);
+            await Room.StartCountdown(new MatchmakingStatusCountdown
+            {
+                Status = state.RoomStatus,
+                TimeRemaining = TimeSpan.FromSeconds(30)
+            }, selectBeatmap);
+        }
+
+        private async Task selectBeatmap(ServerMultiplayerRoom _)
+        {
+            state.RoomStatus = MatchmakingRoomStatus.WaitForSelection;
+            await Hub.NotifyMatchRoomStateChanged(Room);
+            await Room.StartCountdown(new MatchmakingStatusCountdown
+            {
+                Status = state.RoomStatus,
+                TimeRemaining = TimeSpan.FromSeconds(15)
+            }, beginPlay);
+        }
+
+        private async Task beginPlay(ServerMultiplayerRoom _)
+        {
+            state.RoomStatus = MatchmakingRoomStatus.WaitForStart;
+
+            // Todo: Select a beatmap here...
+
+            await Hub.NotifyMatchRoomStateChanged(Room);
+            await Room.StartCountdown(new MatchmakingStatusCountdown
+            {
+                Status = state.RoomStatus,
+                TimeRemaining = TimeSpan.FromSeconds(5)
+            }, Hub.StartMatch);
         }
 
         public override MatchStartedEventDetail GetMatchDetails() => new MatchStartedEventDetail
