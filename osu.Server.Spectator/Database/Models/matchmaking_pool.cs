9a77a36347ed007597a093e009adb56302c2410a
Dan Balasescu <smoogipoo@smgi.me>
2025-11-04 09:02:07 +0900

    Move matchmaking rating-related vars to database

--- osu.Server.Spectator/Database/Models/matchmaking_pool.cs
+++ osu.Server.Spectator/Database/Models/matchmaking_pool.cs
@@ -16,7 +16,21 @@ namespace osu.Server.Spectator.Database.Models
         public int variant_id { get; set; }
         public string name { get; set; } = string.Empty;
         public bool active { get; set; }
+
+        /// <summary>
+        /// The number of players required for a match to be found.
+        /// </summary>
         public int lobby_size { get; set; }
+
+        /// <summary>
+        /// The initial rating search radius.
+        /// </summary>
+        public int rating_search_radius { get; set; }
+
+        /// <summary>
+        /// The amount of time (in seconds) before each doubling of the <see cref="rating_search_radius">rating search radius</see>.
+        /// </summary>
+        public int rating_search_radius_exp { get; set; }
 
         public MatchmakingPool ToMatchmakingPool() => new MatchmakingPool
         {
