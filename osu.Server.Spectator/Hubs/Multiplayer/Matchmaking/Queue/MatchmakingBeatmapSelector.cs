24052f26afedfbc1ea17b7796955314bb8ac930a
Dan Balasescu <smoogipoo@smgi.me>
2025-11-20 16:25:05 +0900

    Further iterate on beatmap selection algorithm

--- osu.Server.Spectator/Hubs/Multiplayer/Matchmaking/Queue/MatchmakingBeatmapSelector.cs
+++ osu.Server.Spectator/Hubs/Multiplayer/Matchmaking/Queue/MatchmakingBeatmapSelector.cs
@@ -2,7 +2,6 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using osu.Server.Spectator.Database;
@@ -48,75 +47,17 @@ namespace osu.Server.Spectator.Hubs.Multiplayer.Matchmaking.Queue
         /// <param name="ratings">The lobby user ratings.</param>
         public matchmaking_pool_beatmap[] GetAppropriateBeatmaps(EloRating[] ratings)
         {
-            double muAvg = ratings.Average(r => r.Mu);
-            // 80 is a sane non-zero default value. It is equal to EloSystem.SigLimit.
-            double muSig = Math.Max(80, Math.Sqrt(ratings.Average(v => Math.Pow(v.Mu - muAvg, 2))));
-
-            List<matchmaking_pool_beatmap> result = [];
-
-            // 25% of beatmaps will be easy for the lobby.
-            result.AddRange(collectBeatmaps(beatmaps, PoolSize / 4, muAvg - muSig, muSig));
-
-            // 25% of beatmaps will be hard for the lobby.
-            result.AddRange(collectBeatmaps(beatmaps.Except(result), PoolSize / 4, muAvg + muSig, muSig));
-
-            // The rest will be of average difficulty.
-            result.AddRange(collectBeatmaps(beatmaps.Except(result), PoolSize - result.Count, muAvg, muSig));
-
-            return result.ToArray();
-        }
-
-        /// <summary>
-        /// Gradually selects a set of beatmaps with an expanding rating window.
-        /// </summary>
-        /// <param name="beatmaps">The available beatmaps.</param>
-        /// <param name="countToAdd">The number of beatmaps to be added.</param>
-        /// <param name="ratingMu">The window rating mean.</param>
-        /// <param name="ratingSig">The window rating variance.</param>
-        private static matchmaking_pool_beatmap[] collectBeatmaps(IEnumerable<matchmaking_pool_beatmap> beatmaps, int countToAdd, double ratingMu, double ratingSig)
-        {
-            // Algorithm:
-            // - Start at the given rating and gradually expand a window outwards.
-            // - Each iteration adds a set of beatmaps to the resultant set.
-            // - The process repeats with the window size and number of items added increasing,
-            //   until the required number of beatmaps have been added.
-            //
-            // Start with 3 standard deviations, adding a maximum of 1 item for each one.
-            //
-            // For example, with a variance of 100:
-            //   Iteration 1: adds 1 item within each of ±100, ±200, and ±300 rating.
-            //   Iteration 2: adds 2 items within each of ±100, ±200, ±300, and ±400 rating.
-            //
-            // This biases the difficulty of items added to be closer to the mean than the outer extremes while still allowing some variation.
-
-            HashSet<matchmaking_pool_beatmap> available = [..beatmaps];
-            List<matchmaking_pool_beatmap> result = [];
-
-            int windowSize = 3;
-            int itemsToAdd = 1;
-
-            while (available.Count > 0 && result.Count < countToAdd)
-            {
-                for (int iteration = 0; iteration < windowSize; iteration++)
-                {
-                    double windowSig = ratingSig * (iteration + 1);
-
-                    matchmaking_pool_beatmap[] windowItems = available.Where(b => Math.Abs((b.rating ?? 1500) - ratingMu) <= windowSig).ToArray();
-                    Random.Shared.Shuffle(windowItems);
-
-                    foreach (matchmaking_pool_beatmap item in windowItems.Take(itemsToAdd))
-                    {
-                        result.Add(item);
-                        available.Remove(item);
-                    }
-                }
-
-                // Next iteration considers slightly more beatmaps.
-                itemsToAdd++;
-                windowSize++;
-            }
-
-            return result.Take(countToAdd).ToArray();
+            return ratings.SelectMany(r =>
+                              beatmaps.OrderByDescending(b =>
+                              {
+                                  double beatmapRating = b.rating ?? 1500;
+                                  // The clamp acts to both prevent division-by-zero and to give all beatmaps a chance.
+                                  double weight = Math.Clamp(Math.Exp(-Math.Pow(beatmapRating - r.Mu, 2) / (2 * r.Sig * r.Sig)), 0.1, 1);
+                                  return Math.Pow(Random.Shared.NextDouble(), 1.0 / weight);
+                              }).Take(PoolSize))
+                          .OrderBy(_ => Random.Shared.NextDouble())
+                          .Take(PoolSize)
+                          .ToArray();
         }
     }
 }
