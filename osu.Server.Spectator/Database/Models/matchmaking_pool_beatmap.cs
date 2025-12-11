9ea7423303e1c5fe15c0ed860dd66d4f6686a824
Dan Balasescu <smoogipoo@smgi.me>
2025-12-11 16:06:09 +0900

    Fix weird typing

--- osu.Server.Spectator/Database/Models/matchmaking_pool_beatmap.cs
+++ osu.Server.Spectator/Database/Models/matchmaking_pool_beatmap.cs
@@ -12,8 +12,8 @@ namespace osu.Server.Spectator.Database.Models
     public class matchmaking_pool_beatmap : IEquatable<matchmaking_pool_beatmap>
     {
         // matchmaking_pool_beatmaps
-        public int id { get; set; }
-        public int pool_id { get; set; }
+        public uint id { get; set; }
+        public uint pool_id { get; set; }
         public int beatmap_id { get; set; }
         public string? mods { get; set; }
         public int? rating { get; set; }
